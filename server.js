26f1f1d5b9cc1f779cfc2317dbfc8af5e3406a96
danielgm23 <danielgm.webcontato@gmail.com>
2024-12-05 15:27:45 -0300

    last committ

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const formRoutes = require('./src/routes/formRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 
 app.use(cors());
