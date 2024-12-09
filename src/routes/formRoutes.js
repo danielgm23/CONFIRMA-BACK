6e291eefd6ddb5e4bfa5b60736d5e8ad321f19de
danielgm23 <danielgm.webcontato@gmail.com>
2024-12-09 11:04:57 -0300

    testando get api

--- src/routes/formRoutes.js
+++ src/routes/formRoutes.js
@@ -1,23 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../db/database.js'); 
+const db = require("../db/database.js");
 
-
-router.post('/submit', async (req, res) => {
+router.post("/submit", async (req, res) => {
   const { name, phone, adultNames } = req.body;
 
-  console.log('Recebido:', { name, phone, adultNames });
+  console.log("Recebido:", { name, phone, adultNames });
 
   try {
-    await db('FormData').insert({
+    await db("FormData").insert({
       name,
       phone,
-      adult_count: adultNames
+      adult_count: adultNames,
     });
-    res.status(200).json({ message: 'Dados recebidos com sucesso!' });
+    res.status(200).json({ message: "Dados recebidos com sucesso!" });
   } catch (error) {
-    console.error('Erro ao inserir dados:', error);
-    res.status(500).json({ error: 'Erro ao inserir dados' });
+    console.error("Erro ao inserir dados:", error);
+    res.status(500).json({ error: "Erro ao inserir dados" });
+  }
+});
+
+router.get("/form/data", async (req, res) => {
+  try {
+    const data = await db("FormData").select("*");
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Erro ao buscar dados:", error);
+    res.status(500).json({ error: "Erro ao buscar dados" });
   }
 });
 
